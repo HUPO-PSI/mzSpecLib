e2311d12542c56dafe5dfe253d74663d3a5f0496
Eric Deutsch <edeutsch@systemsbiology.org>
2019-10-23 10:54:44 -0700

    Added support for new JSON format and added TSV and CSV formats

--- lib/Spectrum.py
+++ lib/Spectrum.py
@@ -508,7 +508,10 @@ class Spectrum:
         """
 
         #### Set a buffer to fill with string data
-        buffer = ""
+        buffer = ''
+
+        #### Make the format string lower case to facilitate comparisons
+        format = format.lower()
 
         #### If the format is text
         if format == "text":
@@ -523,6 +526,66 @@ class Spectrum:
             for peak in self.peak_list:
                 buffer += "\t".join(peak)+"\n"
 
+        #### If the format is TSV
+        elif format == "tsv" or format == "csv":
+
+            #### Set the appropriate delimiter for format
+            delimiter = "\t"
+            if format == "csv": delimiter = ","
+
+            #### Create the header line and columns line
+            buffer += "# Spectrum attributes\n"
+            buffer += "cv_param_group\taccession\tname\tvalue_accession\tvalue\n"
+
+            for attribute in self.attributes:
+                if len(attribute) == 2:
+                    key,value = attribute
+                    cv_param_group = ''
+                elif len(attribute) == 3:
+                    key,value,cv_param_group = attribute
+                    if format == "csv" and ',' in str(value):
+                        value = '"' + value + '"'
+                else:
+                    print("ERROR: Unsupported number of items in attribute")
+                    print(attribute)
+                    sys.exit(10)
+                components = key.split('|',1)
+                if len(components) == 2:
+                    accession,name = components
+                else:
+                    print("ERROR: Unsupported number of items in components")
+                    print(components)
+                    sys.exit(10)
+                components = str(value).split('|',1)
+                if len(components) == 2:
+                    value_accession,value = components
+                    value = str(value)
+                    if format == "csv" and ',' in value:
+                        value = '"' + value + '"'
+                elif len(components) == 1:
+                    value = str(value)
+                    if format == "csv" and ',' in value:
+                        value = '"' + value + '"'
+                    value_accession = ''
+                else:
+                    print("ERROR: Unsupported number of items in components")
+                    print(components)
+                    sys.exit(10)
+
+                #### Create the data line
+                buffer += delimiter.join([cv_param_group,accession,name,value_accession,value])+"\n"
+
+            #### Create the header line and columns line
+            buffer += "# Peak list\n"
+            buffer += "mz\tintensity\tinterpretation\n"
+
+            #### Write out the peak list
+            for peak in self.peak_list:
+                mz,intensity,interpretation = peak
+                if format == "csv" and ',' in str(interpretation):
+                    interpretation = '"' + interpretation + '"'
+                buffer += delimiter.join([mz,intensity,interpretation])+"\n"
+
         #### If the format is JSON
         elif format == "json":
             mzs = []
@@ -532,7 +595,43 @@ class Spectrum:
                 mzs.append(float(peak[0]))
                 intensities.append(float(peak[1]))
                 interpretations.append(peak[2])
-            spectrum = { "attributes": self.attributes, "mzs": mzs, "intensities": intensities,
+
+            #### Organize the attributes from the simple list into the appropriate JSON format
+            attributes = []
+            for attribute in self.attributes:
+                reformed_attribute = {}
+                if len(attribute) == 2:
+                    key,value = attribute
+                elif len(attribute) == 3:
+                    key,value,cv_param_group = attribute
+                    reformed_attribute['cv_param_group'] = cv_param_group
+                else:
+                    print("ERROR: Unsupported number of items in attribute")
+                    print(attribute)
+                    sys.exit(10)
+                components = key.split('|',1)
+                if len(components) == 2:
+                    accession,name = components
+                    reformed_attribute['accession'] = accession
+                    reformed_attribute['name'] = name
+                else:
+                    print("ERROR: Unsupported number of items in components")
+                    print(components)
+                    sys.exit(10)
+                components = str(value).split('|',1)
+                if len(components) == 2:
+                    value_accession,value = components
+                    reformed_attribute['value_accession'] = value_accession
+                    reformed_attribute['value'] = value
+                elif len(components) == 1:
+                    reformed_attribute['value'] = value
+                else:
+                    print("ERROR: Unsupported number of items in components")
+                    print(components)
+                    sys.exit(10)
+                attributes.append(reformed_attribute)
+
+            spectrum = { "attributes": attributes, "mzs": mzs, "intensities": intensities,
                 "interpretations": interpretations }
             buffer = json.dumps(spectrum,sort_keys=True,indent=2)
 
