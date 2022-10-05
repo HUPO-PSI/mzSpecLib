9ce33fd5e7a830f1bb7720997f913d0fe241ca63
Joshua Klein <mobiusklein@gmail.com>
2022-10-04 22:55:37 -0400

    Add example from massivekb

--- implementations/python/mzlib/tools/cli.py
+++ implementations/python/mzlib/tools/cli.py
@@ -77,7 +77,7 @@ def build_index(inpath):
 @click.argument('inpath', type=click.Path(exists=True))
 @click.argument("outpath", type=click.Path())
 @click.option("-f", "--format", type=click.Choice(["text", "json"]), default="text")
-@click.option("-k", "--library-attribute", "library_attributes", type=(str, str), repeat=True, help="Specify an attribute to add to the library metadata section. May be repeated.")
+@click.option("-k", "--library-attribute", "library_attributes", type=(str, str), multiple=True, help="Specify an attribute to add to the library metadata section. May be repeated.")
 def convert(inpath, outpath, format=None, header_file=None, library_attributes=()):
     '''Convert a spectral library from one format to another. If `outpath` is `-`,
     instead of writing to file, data will instead be sent to STDOUT.
